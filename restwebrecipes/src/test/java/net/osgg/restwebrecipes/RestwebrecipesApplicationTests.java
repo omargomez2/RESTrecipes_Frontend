03a32833e4e6953eaec6bc477711dd2b8ed59534
Omar G. <omargomez2@gmail.com>
2020-07-12 12:08:55 -0500

    v0

---
+++ restwebrecipes/src/test/java/net/osgg/restwebrecipes/RestwebrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.osgg.restwebrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebrecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
